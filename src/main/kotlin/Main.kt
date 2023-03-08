3b239775f7b81b00bf67e4c64e3e6292c583f9cf
serah wanjiru <WanjiruMburuuu@gmail.com>
2023-03-08 11:22:56 +0300

    strings-revision

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+fun main() {
+    campus()
+    names("Serah",18)
+    naMe()
+
+}
+fun campus() {
+    var school = "akirachix"
+    println("${school[0]}${school[2]}${school[3]}")
+}
+fun names(name:String,age:Int){
+    println("My name is $name and i am $age years old")
+    NamE()
+
+}
+fun naMe(){
+    var x="Nairobi"
+    println(x.length)
+}
+fun NamE(){
+    var name="serah"
+    if (name=="serah")
+        println("That's me")
+    else
+        println("I dont know who that is")
+}
